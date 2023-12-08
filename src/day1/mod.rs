8ea567e305f825f45d85a471d5081ff5100b974a
elton-cs <govada.elton@gmail.com>
2023-12-08 06:45:28 -0600

    day1a: extract number from string

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{BufReader, BufRead}};
+use std::{fs::File, io::{BufReader, BufRead}, fmt::Error};
 
 pub fn puzzle_a () {
     let path = "src/day1/input_example.txt";
@@ -10,6 +10,18 @@ pub fn puzzle_a () {
     for line in reader.lines() {
         let line = line.expect("unable to read line");
         string_vec.push(line);
-        println!("{:#?}", string_vec);
     }
+    println!("{:#?}", string_vec);
+    
+    let answer: Vec<Vec<String>>= string_vec.iter().map(|s| extract_nums(s).unwrap()).collect();
+    println!("{:#?}", answer);
+
+}
+
+pub fn extract_nums(single_string: &String) -> Result<Vec<String>, Error>  {
+
+    let num_vec_str: Vec<&str> = single_string.matches(char::is_numeric).collect();
+    let num_vec = num_vec_str.iter().map(|s| s.to_string()).collect();
+
+    Ok(num_vec)
 }
