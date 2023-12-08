be504021c91f3d1b9aaaaa5f9bf7e972f04d1620
elton-cs <govada.elton@gmail.com>
2023-12-08 16:37:20 -0600

    day1b: extract spelled digits and idx from string

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{BufReader, BufRead}, fmt::Error};
+use std::{fs::File, io::{BufReader, BufRead}, fmt::Error, usize};
 
 pub fn puzzle_a () {
     let path: &str = "src/day1/input_real.txt";
@@ -38,4 +38,34 @@ fn create_num_string(vec_of_nums: &Vec<String>) -> String {
     number_string.push_str(vec_of_nums[0].as_str());
     number_string.push_str(vec_of_nums[last_idx].as_str());
     number_string
+}
+
+pub fn puzzle_b () {
+    let path: &str = "src/day1/input_example_b.txt";
+    let file: File = File::open(path).expect("could not open file");
+    let reader: BufReader<File> = BufReader::new(file);
+
+    let mut string_vec: Vec<String> = Vec::new();
+
+    for line in reader.lines() {
+        let line: String = line.expect("unable to read line");
+        string_vec.push(line);
+    }
+
+    let answer: Vec<(String, usize)> = extract_spell_nums(&string_vec[3]).unwrap();
+    println!("{:#?}", answer);
+}
+
+fn extract_spell_nums(single_string: &String) -> Result<Vec<(String, usize)>, Error> {
+    let to_match_spelled = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine" ];
+    let mut result: Vec<(String, usize)> = Vec::new();
+
+    for number_in_text in to_match_spelled {
+        // let index = single_string.find(number).unwrap();
+        if let Some(index) = single_string.find(number_in_text) {
+            result.push((number_in_text.to_string(), index))
+        }
+    }
+    
+    Ok(result)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,5 @@ mod day1;
 
 fn main() {
     day1::puzzle_a();
+    day1::puzzle_b();
 }
