fc2b490de2253a553a7150ce6a64652c69b50c5d
elton-cs <govada.elton@gmail.com>
2023-12-08 07:20:31 -0600

    day1a: COMPLETE

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -11,17 +11,7 @@ pub fn puzzle_a () {
         let line: String = line.expect("unable to read line");
         string_vec.push(line);
     }
-    
-    let answer: Vec<Vec<String>>= string_vec.iter().map(|s| extract_digit_string(s).unwrap()).collect();
 
-    let answer: Vec<String> = answer.iter().map(|s| create_num_string(s)).collect();
-
-    let answer: Vec<u32> = answer.iter().map(|s| s.parse::<u32>().unwrap()).collect();
-
-    let answer: u32 = answer.iter().sum();
-    println!("{:#?}", answer);
-
-    // Refactor solution
     let answer: Vec<u32> = string_vec.iter().map(|s| {
         let digit_string_vec = extract_digit_string(s).unwrap();
         let num_string_vec = create_num_string(&digit_string_vec);
@@ -31,15 +21,12 @@ pub fn puzzle_a () {
 
     let answer: u32 = answer.iter().sum();
     println!("{:#?}", answer);
-
-    
 }
 
 fn extract_digit_string(single_string: &String) -> Result<Vec<String>, Error>  {
 
     let num_vec_str: Vec<&str> = single_string.matches(char::is_numeric).collect();
     let num_vec = num_vec_str.iter().map(|s| s.to_string()).collect();
-
     Ok(num_vec)
 }
 
@@ -50,6 +37,5 @@ fn create_num_string(vec_of_nums: &Vec<String>) -> String {
     let mut number_string: String = String::new();
     number_string.push_str(vec_of_nums[0].as_str());
     number_string.push_str(vec_of_nums[last_idx].as_str());
-    
     number_string
 }
